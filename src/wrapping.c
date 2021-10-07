4f57b042e5e42e228f6e32a8897eb852b66b5a76
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2021-10-08 01:17:49 +0200

    Revert

--- src/wrapping.c
+++ src/wrapping.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-#include <limits.h>
-
-#define INITIAL (UINT_MAX / 8 + 2)
+#include <inttypes.h>
 
 int main() {
-	unsigned int i = INITIAL;
-
-	while (i >= INITIAL) {
-		printf("%u >= %u\n", i, INITIAL);
-		i *= 2;
-	}
-	printf("%u < %u\n", i, INITIAL);
+	uint64_t denom = UINT64_MAX/2+1;
+	printf("denom = %"PRIu64"\n", denom);
+	denom *= 2;
+	printf("denom = %"PRIu64"\n", denom);
+	if (denom == 0)
+		printf("WHAT?!?\n");
 }
